5e565514a53b12adc3b332c2ed4b26f2088d0bb1
clzGo <clz1462032600@163.com>
2016-11-09 20:44:34 +0800

    哈哈

--- app/src/main/java/com/share/clz/test999/MainActivity.java
+++ app/src/main/java/com/share/clz/test999/MainActivity.java
@@ -3,6 +3,7 @@ package com.share.clz.test999;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,5 +18,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
     }
 }
